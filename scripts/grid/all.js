49f93660676e2534b789a71366bce584838175b4
FolkvangrForgent <25625121+FolkvangrForgent@users.noreply.github.com>
2025-05-10 22:05:27 -0400

    Initial Plugin Release

---
+++ scripts/grid/all.js
@@ -0,0 +1,152 @@
+export function MeasuredTemplate_object__refreshTemplate(wrapped, self) {
+	// calculate ui scale
+	const scale = (canvas?.dimensions?.uiScale ?? 1);
+	// clear and reuse graphic
+	const graphic = self.template.clear();
+	// line style
+	graphic.lineStyle(self._borderThickness * scale, self.document.borderColor, 0.75).beginFill(0x000000, 0.0);
+	// fill color or texture
+	if ( self.texture ) {
+		graphic.beginTextureFill({texture: self.texture});
+	} else {
+		graphic.beginFill(0x000000, 0.0);
+	}
+	// draw the a custom line and point area shapes
+	if (self.areaShape === 'line') {
+		let shape = self.shape;
+		graphic.drawShape(shape);
+	} else if (self.areaShape === 'point') {
+		if (canvas?.grid?.isSquare) {
+			const dimension = self.document.distance * canvas.dimensions.distancePixels;
+			graphic.drawShape(new PIXI.Rectangle(-dimension, -dimension, dimension * 2, dimension * 2));
+		} else if (canvas?.grid?.isHexagonal) {
+			let shape;
+			const r = 4 / 7 * canvas.grid.size;
+			if ( !canvas.grid.columns ) {
+				const x0 = r * (Math.SQRT3 / 2);
+				const x1 = -x0;
+				const y0 = r;
+				const y1 = y0 / 2;
+				const y2 = -y1;
+				const y3 = -y0;
+				shape = [{x: 0, y: y0}, {x: x1, y: y1}, {x: x1, y: y2},
+					{x: 0, y: y3}, {x: x0, y: y2}, {x: x0, y: y1}];
+			} else {
+				const y0 = r * (Math.SQRT3 / 2);
+				const y1 = -y0;
+				const x0 = r;
+				const x1 = x0 / 2;
+				const x2 = -x1;
+				const x3 = -x0;
+				shape = [{x: 0 + x0, y: 0}, {x: x1, y: y0}, {x: x2, y: y0},
+					{x: 0 + x3, y: 0}, {x: x2, y: y1}, {x: x1, y: y1}];
+			}
+			graphic.drawShape(new PIXI.Polygon(shape));
+		} else {
+			graphic.drawShape(self.shape);
+		}
+	} else {
+		graphic.drawShape(self.shape);
+	}
+	// draw origin point
+	graphic.lineStyle(self._borderThickness * scale, 0x000000).beginFill(0x000000, 0.5).drawCircle(0, 0, 6 * scale).endFill();
+	// draw destination point if areaShape is a line
+	if (self.areaShape === 'line') {
+		graphic.lineStyle(self._borderThickness * scale, 0x000000).beginFill(0x000000, 0.5).drawCircle(self.ray.dx, self.ray.dy, 6 * scale).endFill();
+	}
+}
+
+export function MeasuredTemplate_object__refreshRulerText(wrapped, self) {
+	// get template information
+	if ( self.document.t === 'rect' ) {
+		return wrapped();
+	} else {
+		if (self.areaShape == 'point') {
+			self.ruler.text = ``;
+		} else if (self.areaShape == 'line') {
+			self.ruler.text = `${(Math.round(self.document.distance / canvas.grid.distance) * canvas.grid.distance)}${canvas.grid.units}`;
+			if (Math.round(self.document.width / canvas.grid.distance) > 1) {
+				self.ruler.text += ` x ${(Math.round(self.document.width / canvas.grid.distance) * canvas.grid.distance)}${canvas.grid.units} `;
+			}
+		} else {
+			self.ruler.text = `${(Math.round(self.document.distance / canvas.grid.distance) * canvas.grid.distance)}${canvas.grid.units}`;
+		}
+	}
+	// check where to render ruler text
+	if (self.areaShape === 'line') {
+		let offset;
+		if (canvas?.grid?.isSquare) {
+			offset = foundry.canvas.geometry.Ray.fromAngle(self.document.x, self.document.y, Math.toRadians(self.document.direction), (self.document.distance + (canvas.grid.distance / 2)) * canvas.dimensions.distancePixels);
+			self.ruler.position.set(self.ray.dx + (-self.ruler.width / 2), self.ray.dy + (self.ruler.height / 2));
+		} else {
+			offset = new foundry.canvas.geometry.Ray({x: self.document.x, y: self.document.y}, canvas.grid.getTranslatedPoint({x: self.document.x, y: self.document.y}, self.document.direction, self.document.distance + (canvas.grid.distance / 2)));
+		}
+		self.ruler.position.set(offset.dx + (-self.ruler.width / 2), offset.dy + (self.ruler.height / 2));
+	} else {
+		const offset = foundry.canvas.geometry.Ray.fromAngle(0, 0, self.ray.angle, 75);
+		self.ruler.position.set(offset.dx + (-self.ruler.width / 2), offset.dy + (self.ruler.height / 2));
+	}
+}
+
+export function MeasuredTemplate_layer__onDragLeftStart(wrapped, self, event) {
+	self.clearPreviewContainer();
+	const interaction = event.interactionData;
+	// Snap the origin to the grid
+	if ( !event.shiftKey ) interaction.origin = self.getSnappedPoint(interaction.origin);
+	// Create a pending MeasuredTemplateDocument
+	const tool = game.activeTool;
+	const previewData = {
+		user: game.user.id,
+		t: ((tool === 'emanation') || (tool === 'burst') || (tool === 'point')) ? 'circle' : (tool === 'line') ? 'ray' : tool,
+		x: interaction.origin.x,
+		y: interaction.origin.y,
+		sort: Math.max(self.getMaxSort() + 1, 0),
+		distance: 1,
+		direction: 0,
+		fillColor: game.user.color || '#FF0000',
+		hidden: event.altKey,
+		flags: {
+			pf2e: {
+				areaShape: tool
+			}
+		}
+	};
+	const defaults = CONFIG.MeasuredTemplate.defaults;
+	if ( tool === 'cone' ) {
+		try {
+			if (canvas?.grid?.isGridless) {
+				previewData.angle = game.settings.get('f2e-grid-enhancements', 'gridless-cone-template-angle');
+			} else if (canvas?.grid?.isHexagonal) {
+				previewData.angle = game.settings.get('f2e-grid-enhancements', 'hex-cone-template-angle');
+			} else if (canvas?.grid?.isSquare) {
+				previewData.angle = game.settings.get('f2e-grid-enhancements', 'square-cone-template-angle');
+			} else {
+				previewData.angle = defaults.angle;
+			}
+		} catch {
+			previewData.angle = defaults.angle;
+		}
+	} else if ( tool === 'line' || tool === 'ray' ) {
+		previewData.width = (CONFIG.MeasuredTemplate.defaults.width * canvas.dimensions.distance);
+	}
+	const cls = foundry.utils.getDocumentClass('MeasuredTemplate');
+	const doc = new cls(previewData, {parent: canvas.scene});
+	// Create a preview MeasuredTemplate object
+	const template = new self.constructor.placeableClass(doc);
+	doc._object = template;
+	interaction.preview = self.preview.addChild(template);
+	template.draw();
+}
+
+export function MeasuredTemplate_layer__onDragLeftMove(wrapped, self, event) {
+	const interaction = event.interactionData;
+	// Update the preview object direction
+	if (interaction.preview.areaShape == 'point') {
+		const ray = new foundry.canvas.geometry.Ray(interaction.origin, interaction.destination);
+		interaction.preview.document.direction = Math.normalizeDegrees(Math.toDegrees(ray.angle));
+		interaction.preview.document.distance = canvas.grid.distance / 2;
+		interaction.preview.renderFlags.set({refreshShape: true});
+	} else {
+		wrapped(event);
+	}
+}
---
+++ scripts/grid/hex.js
@@ -0,0 +1,211 @@
+// TODO support 3D hex columns
+export function Token_object_distanceTo(wrapped, self, target, opts) {
+	// if target is self distance will always be 0
+	if (self === target) return 0;
+	// calculate the self points
+	const selfPoints = []
+	for (const offset of self.document.getOccupiedGridSpaceOffsets()) {
+		const point = canvas.grid.getCenterPoint({i: offset.i, j: offset.j})
+		point.elevation = self.document.elevation
+		selfPoints.push(point);
+	}
+	// calculate the target points
+	const targetPoints = []
+	if (target instanceof CONFIG.Token.objectClass) {
+		for (const offset of target.document.getOccupiedGridSpaceOffsets()) {
+			const point = canvas.grid.getCenterPoint({i: offset.i, j: offset.j})
+			point.elevation = target.document.elevation
+			targetPoints.push(point);
+		}
+	} else {
+		// center point (and add elevation until PF2e point is 3D)
+		targetPoints.push(canvas.grid.getCenterPoint({x: target.x, y: target.y, elevation: 0}));
+	}
+	// calculate minimum distance
+	let distance = Infinity;
+	for (const origin of selfPoints) {
+		for (const destination of targetPoints) {
+			const distanceCandidate = canvas.grid.measurePath([origin, destination]).distance;
+			if (distanceCandidate < distance) {
+				distance = distanceCandidate;
+			}
+		}
+	}
+	// process reach
+	distance -= opts?.reach ?? 0;
+	// remove negative distances
+	if (distance < 0) {
+		distance = 0;
+	}
+	// return distance
+	return distance;
+}
+
+export function MeasuredTemplate_object__computeShape(wrapped, self) {
+	switch (self.document.t) {
+		case 'circle':
+			return new PIXI.Polygon(canvas.grid.getCircle({x: 0, y: 0}, self.document.distance));
+		case 'cone':
+			return new PIXI.Polygon(canvas.grid.getCone({x: 0, y: 0}, self.document.distance, self.document.direction, self.document.angle));
+		case 'ray':
+			const endpoint = canvas.grid.getTranslatedPoint({x: 0, y: 0}, self.document.direction, self.document.distance);
+			return new PIXI.Polygon(0, 0, endpoint.x, endpoint.y);
+		default:
+			return wrapped();
+	}
+}
+
+function hexPath(origin, direction, steps) {
+	// do as much math at the begining
+	const r = Math.toRadians(direction);
+	const sin_r = Math.sin(r);
+	const cos_r = Math.cos(r);
+	const a = -sin_r;
+	const b = cos_r;
+	const c = sin_r * origin.x - cos_r * origin.y;
+	const sqrt_aa_bb = Math.sqrt(a * a + b * b);
+	// calculate which grid section the direction points into
+	const direction_section = (Math.round((direction - ((canvas.grid.type == CONST.GRID_TYPES.HEXODDQ || canvas.grid.type == CONST.GRID_TYPES.HEXEVENQ) ? 60 : 30)) / 60) + 1) % 6;
+	// keep track of hex positions found for each step
+	const path = [];
+	// calculate initial hex add it to path
+	let previous_hex = canvas.grid.getCube({x: origin.x, y: origin.y});
+	path.push(previous_hex);
+	// iterate for number of steps
+	for (let i = 1; i < steps; i++) {
+		let hex = undefined;
+		let distance = Infinity;
+		let candidate_hexs = [];
+		// get two candidate hexs based on direction section
+		if (direction_section == 0) {
+			candidate_hexs = [{q: (previous_hex.q + 1), r: (previous_hex.r - 1), s: (previous_hex.s)},{q: (previous_hex.q + 1), r: (previous_hex.r), s: (previous_hex.s - 1)}];
+		} else if (direction_section == 1) {
+			candidate_hexs = [{q: (previous_hex.q + 1), r: (previous_hex.r), s: (previous_hex.s - 1)},{q: (previous_hex.q), r: (previous_hex.r + 1), s: (previous_hex.s - 1)}];
+		} else if (direction_section == 2) {
+			candidate_hexs = [{q: (previous_hex.q), r: (previous_hex.r + 1), s: (previous_hex.s - 1)},{q: (previous_hex.q - 1), r: (previous_hex.r + 1), s: (previous_hex.s)}];
+		} else if (direction_section == 3) {
+			candidate_hexs = [{q: (previous_hex.q - 1), r: (previous_hex.r + 1), s: (previous_hex.s)},{q: (previous_hex.q - 1), r: (previous_hex.r), s: (previous_hex.s + 1)}];
+		} else if (direction_section == 4) {
+			candidate_hexs = [{q: (previous_hex.q - 1), r: (previous_hex.r), s: (previous_hex.s + 1)},{q: (previous_hex.q), r: (previous_hex.r - 1), s: (previous_hex.s + 1)}];
+		} else if (direction_section == 5) {
+			candidate_hexs = [{q: (previous_hex.q), r: (previous_hex.r - 1), s: (previous_hex.s + 1)},{q: (previous_hex.q + 1), r: (previous_hex.r - 1), s: (previous_hex.s)}];
+		}
+		// check which candidate_hexs is closer to line
+		for (const candidate_hex of candidate_hexs) {
+			const candidate_point = canvas.grid.getCenterPoint(candidate_hex);
+			const point_intersection_distance = Math.abs((a * candidate_point.x + b * candidate_point.y + c) / sqrt_aa_bb)
+			if (point_intersection_distance < distance) {
+				hex = candidate_hex;
+				distance = point_intersection_distance;
+			}
+		}
+		// safety check
+		if (distance !== Infinity) {
+			path.push(hex);
+			previous_hex = hex;
+		} else {
+			break;
+		}
+	}
+	return path;
+}
+
+export function MeasuredTemplate_object__getGridHighlightPositions(wrapped, self) {
+	if (self.areaShape === 'line') {
+		// calculate how many hexs wide the template is
+		const hex_width = Math.round(self.document.width / canvas.dimensions.distance);
+		// calculate how many hexs long the template is
+		const hex_length = Math.round(self.document.distance / canvas.grid.distance);
+		// keep track of positions
+		const highlight_positions = [];
+		// determine center point of closest hex
+		const center_point = canvas.grid.getCenterPoint({x: self.document.x, y: self.document.y});
+		// ranges from 0 to 4*pi
+		let e = (Math.toRadians(self.document.direction) - Math.atan2((center_point.y - self.document.y), (center_point.x - self.document.x)) + Math.PI);
+		while (e > Math.PI) {
+			e -= 2 * Math.PI;
+		}
+		const offset_point = canvas.grid.getTranslatedPoint({x: self.document.x, y: self.document.y}, self.document.direction + ((e < 0) ? (90) : (-90)), Math.floor(hex_width / 2) * canvas.grid.distance);
+		const delta_point = canvas.grid.getTranslatedPoint({x: 0, y: 0}, self.document.direction + ((e < 0) ? (-90) : (90)), canvas.grid.distance);
+		for (let i = 0; i < hex_width; i++) {
+			// canvas.ping({x: (x + i * dx), y: (y + i * dy)});
+			for (const hex of hexPath({x: (offset_point.x + i * delta_point.x), y: (offset_point.y + i * delta_point.y)}, self.document.rotation, hex_length)) {
+				highlight_positions.push(canvas.grid.getTopLeftPoint(hex));
+			}
+		}
+		return highlight_positions;
+	} else {
+		return wrapped();
+	}
+}
+
+export function MeasuredTemplate_layer_getSnappedPoint(wrapped, self, point) {
+	return point;
+}
+
+export function MeasuredTemplate_object__refreshPosition(wrapped, self) {
+	// determine snapping mode
+	let snappingMode = 0;
+	switch (self.areaShape) {
+		case 'point':
+			snappingMode = CONST.GRID_SNAPPING_MODES.CENTER;
+			break;
+		case 'emanation':
+			snappingMode = CONST.GRID_SNAPPING_MODES.CENTER | CONST.GRID_SNAPPING_MODES.VERTEX;
+			break;
+		case 'burst':
+			snappingMode = CONST.GRID_SNAPPING_MODES.VERTEX;
+			break;
+		case 'cone':
+			snappingMode = CONST.GRID_SNAPPING_MODES.CENTER | CONST.GRID_SNAPPING_MODES.EDGE_MIDPOINT | CONST.GRID_SNAPPING_MODES.VERTEX;
+			break;
+		case 'line':
+		default:
+			break;
+	}
+	// get snapped position based on snapping mode
+	const origin = canvas.grid.getSnappedPoint({x: self.document.x, y: self.document.y}, {mode: snappingMode, resolution: 1});
+	// set document to snapped position
+	self.document.x = origin.x;
+	self.document.y = origin.y;
+	// call wrapped function to continue logic
+	wrapped();
+}
+
+export function MeasuredTemplate_object__refreshShape(wrapped, self) {
+	self.ray = new foundry.canvas.geometry.Ray({x: self.document.x, y: self.document.y}, canvas.grid.getTranslatedPoint({x: self.document.x, y: self.document.y}, self.document.direction, self.document.distance));
+	self.shape = self._computeShape();
+}
+
+export function MeasuredTemplate_layer__onMouseWheel(wrapped, self, event) {
+	// try and get a hovered template
+	const template = self.hover;
+	// determine if there is a template that is not a preview
+	if ( !template || template.isPreview ) return;
+	// determine the incremental angle of rotation from event data
+	const snap = event.shiftKey ? 30 : 15;
+	// turn mouse wheel delta into angle delta
+	const delta = snap * Math.sign(event.delta);
+	// return rotation based on snap
+	return template.rotate(template.document.direction + delta, snap);
+}
+
+export function MeasuredTemplate_layer__onDragLeftMove(wrapped, self, event) {
+	const interaction = event.interactionData;
+	// Snap the destination to the grid
+	if ( !event.shiftKey ) interaction.destination = self.getSnappedPoint(interaction.destination);
+	// Update the preview object direction
+	const ray = new foundry.canvas.geometry.Ray(interaction.origin, interaction.destination);
+	if (interaction.preview.areaShape == 'cone') {
+		const snapAngle = Math.PI / 6;
+		interaction.preview.document.direction = Math.normalizeDegrees(Math.toDegrees(Math.floor(ray.angle / snapAngle + 0.5) * snapAngle));
+	} else {
+		interaction.preview.document.direction = Math.normalizeDegrees(Math.toDegrees(ray.angle));
+	}
+	if (interaction.preview.areaShape == 'point') {
+		interaction.preview.document.distance = canvas.grid.distance / 2;
+	} else {
+		interaction.preview.document.distance = Math.max(Math.round(canvas.grid.measurePath([interaction.origin, interaction.destination]).distance / canvas.grid.distance) * canvas.grid.distance, canvas.grid.distance);
+	}
+	interaction.preview.renderFlags.set({refreshShape: true});
+}
